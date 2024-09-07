1a373c55b6aad65af72136f662e0c19b42d74ffa
alefcristo <contato.alefcristo@gmail.com>
2024-09-07 18:09:40 -0300

    mudança final

--- dados.js
+++ dados.js
@@ -1,20 +1,20 @@
 let dados = [
     {
       titulo: "Pedro Almeida",
-      bairro: "Centro",
       descricao: "Profissional: Capinador autônomo há 5 anos. Atua principalmente em grandes áreas, como fazendas e sítios.",
-      link: "https://wa.me/5511987654321?text="
+      link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20o%20capinador%20master%20Pedro%20Almeida",
+      tags: "Centro Cen",
     },
     {
       titulo: "Mario Silva",
-      bairro: "Centro",
       descricao: "Profissional: Capinador autônomo há 20 anos. Atua principalmente em áreas residenciais e pequenas empresas.",
-      link: "https://wa.me/5511987654321?text="
+      link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20o%20capinador%20master%20Mario%20Silva",
+      tags: "CENTRO CEN",
     },
     {
       titulo: "Maria Santos",
-      bairro: "Tancredo Neves",
       descricao: "Profissional: Capinadora autônomo há 10 anos. Trabalha realizando a preparação do terreno para a implantação de jardins.",
-      link: "https://wa.me/5511987654321?text="
+      link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20o%20capinador%20master%20Maria%20Santos",
+      tags: "Centro Cen",
     }
   ];
