c2260f50def564a9a96244313831f34b32262d5e
alefcristo <contato.alefcristo@gmail.com>
2024-09-07 18:08:22 -0300

    Mudança versão final

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-function pesquisarBairro () {let section = document.getElementById("resultados-pesquisa")
+function pesquisarBairro () {
+    let section = document.getElementById
+    ("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+
+    if (campoPesquisa == "") {
+    section.innerHTML = "<p>Você não digitou um bairro</p>"
+    return
+    }
     
-    let resultados = ""
+campoPesquisa = campoPesquisa.toLowerCase ()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
     
     for (let dado of dados) {
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+     if  (titulo.includes(campoPesquisa) || descricao.
+     includes(campoPesquisa) || tags.includes
+    (campoPesquisa))
+        {resultados += `
         <div class="item-resultado">
         <h2> 
         ${dado.titulo}
@@ -13,11 +34,15 @@ function pesquisarBairro () {let section = document.getElementById("resultados-p
         <a href=${dado.link} target="_blank">Agende um serviço</a>
       </div>
       `
+     }
+        
+     if (!resultados) {
+        resultados = "<p>Seu bairro ainda não possui capinadores cadastrados</p>"
+    }
+
     }
     
     section.innerHTML = resultados
     
 }
-
-
 
