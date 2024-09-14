3a77c601d3c054d6de3e91141322cfbc76c59a2c
alefcristo <contato.alefcristo@gmail.com>
2024-09-14 18:08:14 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,35 +1,23 @@
-function pesquisarBairro() {
-  const campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
-  const resultadosSection = document.getElementById("resultados-pesquisa");
-
-  if (!campoPesquisa) {
-    resultadosSection.innerHTML = "<p class='aviso-erro'>Você não digitou um bairro</p>";
-    return;
-}
-
-
-  const resultados = dados.filter(dado => {
-      const { titulo, descricao, tags } = dado;
-      return titulo.toLowerCase().includes(campoPesquisa) ||
-             descricao.toLowerCase().includes(campoPesquisa) ||
-             tags.toLowerCase().includes(campoPesquisa);
-  });
-
-  let htmlResultados = "";
-
-  if (resultados.length > 0) {
-      resultados.forEach(dado => {
-          htmlResultados += `
-              <div class="item-resultado">
-                  <img src="${dado.imagem}" alt="${dado.titulo}">
-                  <h2>${dado.titulo}</h2>
-                  <p class="descricao-meta">${dado.descricao}</p>
-                  <a href="${dado.link}" target="_blank">Agende um serviço</a>
-              </div>
-          `;
-      });
-    } else {
-      htmlResultados = "<p class='aviso-sem-capinadores'>Bairro sem capinadores cadastrados</p>";
-    };
-    resultadosSection.innerHTML = htmlResultados;
-}
+let dados = [
+  {
+      titulo: "Pedro Almeida",
+      descricao: "Profissional: Capinador autônomo há 5 anos. Atua principalmente em grandes áreas, como fazendas e sítios.",
+      link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20o%20capinador%20master%20Pedro%20Almeida",
+      tags: "Centro Cen",
+      imagem: "https://i.pinimg.com/736x/ce/46/5f/ce465fe00218af8dce7076b6d3935f27.jpg"
+  },
+  {
+      titulo: "Mario Silva",
+      descricao: "Profissional: Capinador autônomo há 20 anos. Atua principalmente em áreas residenciais e pequenas empresas.",
+      link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20o%20capinador%20master%20Mario%20Silva",
+      tags: "Centro Cen",
+      imagem: "https://i.pinimg.com/736x/62/95/1b/62951b87e81305939e6708715d47511d.jpg"
+  },
+  {
+      titulo: "Maria Santos",
+      descricao: "Profissional: Capinadora autônoma há 10 anos. Trabalha realizando a preparação do terreno para a implantação de jardins.",
+      link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20a%20capinadora%20Maria%20Santos",
+      tags: "Centro Cen",
+      imagem: "https://i.pinimg.com/736x/e9/48/7f/e9487fe455f93244482a26e14dd6ddf8.jpg"
+  }
+];
