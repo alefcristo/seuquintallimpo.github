2b44b26a44c3f810e5e0a5cc2eaa6b71716ed5d0
alefcristo <contato.alefcristo@gmail.com>
2024-09-14 18:33:09 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,21 +3,24 @@ let dados = [
       titulo: "Pedro Almeida",
       descricao: "Profissional: Capinador autônomo há 5 anos. Atua principalmente em grandes áreas, como fazendas e sítios.",
       link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20o%20capinador%20master%20Pedro%20Almeida",
-      tags: "Centro Cen",
+      tags: "Capinador, Sítios, Fazendas",
+      bairro: "Centro", // Adiciona o campo bairro
       imagem: "https://i.pinimg.com/736x/ce/46/5f/ce465fe00218af8dce7076b6d3935f27.jpg"
   },
   {
       titulo: "Mario Silva",
       descricao: "Profissional: Capinador autônomo há 20 anos. Atua principalmente em áreas residenciais e pequenas empresas.",
       link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20o%20capinador%20master%20Mario%20Silva",
-      tags: "Centro Cen",
+      tags: "Capinador, Residências, Empresas",
+      bairro: "Centro", // Adiciona o campo bairro
       imagem: "https://i.pinimg.com/736x/62/95/1b/62951b87e81305939e6708715d47511d.jpg"
   },
   {
       titulo: "Maria Santos",
       descricao: "Profissional: Capinadora autônoma há 10 anos. Trabalha realizando a preparação do terreno para a implantação de jardins.",
       link: "https://wa.me/5575992047252?text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20servi%C3%A7o%20com%20a%20capinadora%20Maria%20Santos",
-      tags: "Centro Cen",
+      tags: "Capinadora, Jardins, Preparação de Terreno",
+      bairro: "Centro", // Adiciona o campo bairro
       imagem: "https://i.pinimg.com/736x/e9/48/7f/e9487fe455f93244482a26e14dd6ddf8.jpg"
   }
 ];
