e400adc0efc06ceb83610fd76d26aef63a219146
alefcristo <contato.alefcristo@gmail.com>
2024-09-14 10:59:13 -0300

    ALTERAÇÃO 14/09

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 function pesquisarBairro() {
-    const campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
-    const resultadosSection = document.getElementById("resultados-pesquisa");
-  
-    if (!campoPesquisa) {
+  const campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+  const resultadosSection = document.getElementById("resultados-pesquisa");
+
+  if (!campoPesquisa) {
       resultadosSection.innerHTML = "<p>Você não digitou um bairro</p>";
       return;
-    }
-  
-    const resultados = dados.filter(dado => {
+  }
+
+  const resultados = dados.filter(dado => {
       const { titulo, descricao, tags } = dado;
       return titulo.toLowerCase().includes(campoPesquisa) ||
              descricao.toLowerCase().includes(campoPesquisa) ||
              tags.toLowerCase().includes(campoPesquisa);
-    });
-  
-    let htmlResultados = "";
-  
-    if (resultados.length > 0) {
+  });
+
+  let htmlResultados = "";
+
+  if (resultados.length > 0) {
       resultados.forEach(dado => {
-        htmlResultados += `
-          <div class="item-resultado">
-            <h2>${dado.titulo}</h2>
-            <p class="descricao-meta">${dado.descricao}</p>
-            <a href="${dado.link}" target="_blank">Agende um serviço</a>
-          </div>
-        `;
+          htmlResultados += `
+              <div class="item-resultado">
+                  <img src="${dado.imagem}" alt="${dado.titulo}">
+                  <h2>${dado.titulo}</h2>
+                  <p class="descricao-meta">${dado.descricao}</p>
+                  <a href="${dado.link}" target="_blank">Agende um serviço</a>
+              </div>
+          `;
       });
-    } else {
+  } else {
       htmlResultados = "<p>Seu bairro ainda não possui capinadores cadastrados</p>";
-    }
-  
-    resultadosSection.innerHTML = htmlResultados;
   }
+
+  resultadosSection.innerHTML = htmlResultados;
+}
